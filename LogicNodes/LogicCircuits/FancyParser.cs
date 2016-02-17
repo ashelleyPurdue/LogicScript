dd9b340566ad958fd71b47c6f387e0bcc9ce7ee9
John B Turnip <johnbturnip@gmail.com>
2016-02-17 13:12:57 -0500

    Wrote NextCommand() method.

--- LogicNodes/LogicCircuits/FancyParser.cs
+++ LogicNodes/LogicCircuits/FancyParser.cs
@@ -3,11 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using System.IO;
+
 namespace LogicNodes.LogicCircuits
 {
     public class FancyParser
     {
+        private StreamReader fileReader;
 
+        public FancyParser(string fileName)
+        {
+            //Open the file
+
+            fileReader = new StreamReader(fileName);
+        }
+
+        private string NextCommand()
+        {
+            //Gets the next command, separated by a semicolon
+
+            StringBuilder builder = new StringBuilder();
+
+            char c = (char)(fileReader.Read());
+            while (c != ';')
+            {
+                builder.Append(c);
+                c = (char)(fileReader.Read());
+            }
+
+            return builder.ToString();
+        }
     }
-
 }
