0d05f14e32f701ad6f455eaac71b699662a98f45
John B Turnip <johnbturnip@gmail.com>
2016-02-17 14:51:15 -0500

    Created the basic framework for the class.

--- LogicNodes/LogicCircuits/FancyParser.cs
+++ LogicNodes/LogicCircuits/FancyParser.cs
@@ -4,34 +4,80 @@ using System.Linq;
 using System.Text;
 
 using System.IO;
+using LogicCircuits;
 
 namespace LogicNodes.LogicCircuits
 {
     public class FancyParser
     {
         private StreamReader fileReader;
+        private MasterCircuit masterCircuit;
 
-        public FancyParser(string fileName)
+        public FancyParser(string fileName, string masterCircuitName)
         {
-            //Open the file
+            //Start the master circuit
+            masterCircuit = new MasterCircuit(masterCircuitName);
 
+            //Open the file
             fileReader = new StreamReader(fileName);
+
+            //Parse the file, generating the circuits
+            Parse();
+
+            //Close the reader.
+            fileReader.Close();
+        }
+
+        public MasterCircuit GetMasterCircuit()
+        {
+            return masterCircuit;
+        }
+
+        private void Parse()
+        {
+            //Execute every command.
+
+            string command = NextCommand();
+            while (command != null)
+            {
+                ExecuteCommand(command);
+                command = NextCommand();
+            }
+        }
+
+        private void ExecuteCommand(string command)
+        {
+            //TODO: Execute the command
         }
 
         private string NextCommand()
         {
             //Gets the next command, separated by a semicolon
+            //Returns null if there isn't a next command.
 
             StringBuilder builder = new StringBuilder();
 
-            char c = (char)(fileReader.Read());
-            while (c != ';')
+            while (true)
             {
+                //Return null if end of stream
+                if (fileReader.EndOfStream)
+                {
+                    return null;
+                }
+
+                //Get the next c
+                char c = (char)(fileReader.Read());
+
+                //If c is a semicolon, that's the end of the command.
+                if (c == ';')
+                {
+                    return builder.ToString();
+                }
+
+                //Append c to the builder
                 builder.Append(c);
-                c = (char)(fileReader.Read());
             }
 
-            return builder.ToString();
         }
     }
 }
